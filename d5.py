4fffa8ec7c43d0daed43904ec355ee4f08315dce
Matt Boyd <machallboyd@gmail.com>
2023-07-07 17:50:07 -0700

    Day five, part two

--- d5.py
+++ d5.py
@@ -3,18 +3,35 @@ from collections.abc import Generator
 
 puzzle_input = 'ugkcyxxp'
 
-def passgen(input: str, length: int) -> str:
-    def _passgen() -> Generator[str]:
-        index = 0
-        remaining = length
-        while remaining > 0:
-            seed = input + str(index)
-            seedenc = seed.encode('utf-8')
-            hash = hashlib.md5(seedenc)
-            if (digest := hash.hexdigest())[:5] == '00000':
-                yield(digest[5])
-                remaining -= 1
-            index += 1
-    return ''.join(_passgen())
+class Password:
 
-print(passgen(puzzle_input, 8))
+    def __init__(self, puzzle_input, length):
+        self.puzzle_input = puzzle_input
+        self.length = length
+        self.password = [None for _ in range(length)]
+
+    def assign(self, position, character):
+        try:
+            intpos = int(position)
+            if self.password[intpos] is None:
+                self.password[intpos] = character
+        except ValueError:
+            return
+        except IndexError:
+            return
+
+    def report(self):
+        print(''.join(self.password))
+
+    def passgen(self) -> str:
+        index = 0
+        while None in self.password:
+            seed = self.puzzle_input + str(index)
+            hashed = hashlib.md5(seed.encode('utf-8'))
+            if (digest := hashed.hexdigest())[:5] == '00000':
+                self.assign(position=digest[5], character=digest[6])
+            index += 1
+
+newpass = Password(puzzle_input, 8)
+newpass.passgen()
+newpass.report()
