bcec00e0532e1e5378cd0c21843f4bd6371b667a
machallboyd <machallboyd@gmail.com>
2023-12-05 16:39:10 -0800

    2023 day 1, part 2 trivial optimization

--- 2023/d1b.py
+++ 2023/d1b.py
@@ -4,34 +4,34 @@ from collections.abc import Generator, Iterable
 p = Path(__file__).with_name('d1.txt')
 
 word2number = {
-    '1': 1,
-    '2': 2,
-    '3': 3,
-    '4': 4,
-    '5': 5,
-    '6': 6,
-    '7': 7,
-    '8': 8,
-    '9': 9,
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9
+    '1': '1',
+    '2': '2',
+    '3': '3',
+    '4': '4',
+    '5': '5',
+    '6': '6',
+    '7': '7',
+    '8': '8',
+    '9': '9',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
 }
-def converter(calibration: str) -> Generator[int]:
+def converter(calibration: str) -> Generator[str]:
     for i in range(len(calibration)):
         for test_len in range(1, 6):
             if valid := word2number.get(calibration[i: i+test_len]):
                 yield valid
 
-def firstlast(digitized: Iterable[list[int]]) -> Generator[int]:
+def firstlast(digitized: Iterable[list[str]]) -> Generator[int]:
     for digits in digitized:
-        yield int(str(digits[0])+str(digits[-1]))
+        yield int(digits[0]+digits[-1])
 
 lines = p.open('r').readlines()
 print(sum(firstlast(list(converter(line)) for line in lines)))
