9cb234764443aa936a905d2ce7fd03b98d19134f
machallboyd <machallboyd@gmail.com>
2023-12-05 16:27:32 -0800

    2023 day 1, part 2

--- 2023/d1.py
+++ 2023/d1.py
@@ -1,6 +1,25 @@
+from pathlib import Path
+from collections.abc import Generator, Iterable
 
-def single_digit(calibration: str) -> str:
+p = Path(__file__).with_name('d1.txt')
+
+def single_digit(calibration:Iterable[str]) -> int:
     for c in calibration:
-        if digit := int(c):
+        try:
+            digit = int(c)
+            return digit
+        except (ValueError):
+            pass
 
 def by_digit(calibration: str):
+    first = single_digit(calibration)
+    last = single_digit(reversed(calibration))
+    return int(str(first) + str(last))
+
+def digital_values(calibrations: Iterable) -> Generator[int]:
+    for calibration in calibrations:
+        yield by_digit(calibration=calibration)
+
+lines = p.open('r').readlines()
+print("by digits:")
+print(sum(digital_values(lines)))
